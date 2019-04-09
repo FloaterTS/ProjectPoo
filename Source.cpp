55f1e48bb55fcb5092d84b99fe73a335c4052ea1
FloaterTS <48510737+FloaterTS@users.noreply.github.com>
2019-04-10 00:33:44 +0300

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,390 +1,274 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
-#include <fstream>
-#include <algorithm>
-#include <vector>
-#include <queue>
+#include <string>
 
 using namespace std;
 
-class GrafOr
+int max(int a, int b)
 {
+	return (a < b) ? b : a;
+}
+
+
+class Nod
+{
+
+protected:
+	int info;
+	Nod *st;
+	Nod *dr;
+
+public:
+	Nod(int x = 0) : info(x), st(NULL), dr(NULL) {}
+	Nod(const Nod &n2) : info(n2.info), st(n2.st), dr(n2.dr) {}
+	virtual ~Nod() { info = 0; st = NULL; dr = NULL; }
+
+	friend class Arbore;
+	friend class ABC;
+
+	friend istream &operator >> (istream &is, ABC &abc);
+	friend ostream &operator << (ostream &os, const ABC &abc);
+};
+
+class Nod_rosu_negru : Nod
+{
+
 private:
+	char culoare[6];
+	Nod_rosu_negru *st_rn;
+	Nod_rosu_negru *dr_rn;
 
-	int n, m;
+public:
+	Nod_rosu_negru(const char cul[], int x = 0) : Nod(x), st_rn(NULL), dr_rn(NULL) { strcpy(culoare, cul); }
+	Nod_rosu_negru(const Nod_rosu_negru &nrn2) : Nod(nrn2), st_rn(nrn2.st_rn), dr_rn(nrn2.dr_rn) { strcpy(culoare, nrn2.culoare); }
+	~Nod_rosu_negru() { delete[] culoare; st_rn = NULL; dr_rn = NULL; }
 
-	vector <vector <int>> vfAdiac;
+	friend class Arbore_bicolor;
+};
+
+
+class Arbore
+{
+
+protected:
+	int nr_noduri;
+
+public:
+	Arbore(int nr = 0) : nr_noduri(nr) {}
+	Arbore(const Arbore &a2) : nr_noduri(a2.nr_noduri) {}
+	virtual ~Arbore() { nr_noduri = 0; }
+
+	virtual int height() = 0;
+
+};
+
+
+class ABC : Arbore
+{
+
+private:
+	Nod *rad;
+
+public:
+	ABC() : Arbore(), rad(NULL) {}
+	ABC(int x) : Arbore(1) { rad = new Nod(x); }
+
+	~ABC()
+	{
+		destroy(rad);
+		rad = NULL;
+	}
+	void destroy(Nod *n)
+	{
+		if (n == NULL)
+			return;
+		destroy(n->st);
+		destroy(n->dr);
+		delete n;
+	}
+
+	Nod *copy(Nod *n, const Nod *n2)
+	{
+		if (n2 == NULL)
+			return NULL;
+		if (n == NULL)
+			n = new Nod();
+		n->info = n2->info;
+		n->st = copy(n->st, n2->st);
+		n->dr = copy(n->dr, n2->dr);
+		return n;
+	}
+
+	void add(int x, Nod *n)
+	{
+		if (x < n->info)
+			if (n->st == NULL)
+			{
+				Nod *p = new Nod(x);
+				n->st = p;
+			}
+			else
+				add(x, n->st);
+		else if (x > n->info)
+			if (n->dr == NULL)
+			{
+				Nod *p = new Nod(x);
+				n->dr = p;
+			}
+			else
+				add(x, n->dr);
+		else if (x == n->info)
+			cout << "Nod deja prezent";
+	}
+	void srd(const Nod *n)
+	{
+		if (n == NULL)
+			return;
+		srd(n->st);
+		cout << n->info << " ";
+		srd(n->dr);
+	}
+
+	int height() { return heightHelp(rad); }
+	int heightHelp(Nod *n)
+	{
+		if (n == NULL)
+			return 0;
+
+		return 1 + max(heightHelp(n->st), heightHelp(n->dr));
+	}
+
+	bool Null() { return rad == NULL ? true : false; }
+
+	void operator = (const ABC &abc) { rad = copy(rad, abc.rad); }
+	friend istream &operator >> (istream &is, ABC &abc);
+	friend ostream &operator << (ostream &os, ABC &abc);
+};
+
+class Arbore_bicolor : Arbore
+{
+
+private:
+	Nod_rosu_negru *rad;
 
 public:
 
-	GrafOr();
-	GrafOr(int x) { this->reSetGrOr(x); };
+	Arbore_bicolor() : Arbore(), rad(NULL) {}
+	Arbore_bicolor(int x) : Arbore(1) { rad = new Nod_rosu_negru("negru", x); }
 
-	int getN() { return this->n; };
-	int getM() { return this->m; };
+	int height() { return log2(negre(rad)); }
+	int negre(Nod_rosu_negru *nrn)
+	{
+		if (nrn == NULL)
+			return 0;
+		int ng = 0;
 
-	int **getMat();
+		if (strcpy(nrn->culoare, "negru") == 0)
+			ng = 1;
 
-	void addNod();
-	void bfs(int x);
-	void dfs(int x);
-	void reSetGrOr(int x);
-	void addDrum(int x, int y);
-	void dfsUtil(int x, bool viz[]);
+		ng = +negre(nrn->st_rn);
+		ng = +negre(nrn->dr_rn);
 
-	bool isTareConex();
-	bool drumFromTo(int x, int y);
+		return ng;
+	}
 
-	vector <int> getDrumuri(int i) { return vfAdiac[i]; };
-	vector <vector <int>> compTareConexe();
-
-	///friend istream &operator >> (istream &is, GrafOr &grO);
-	///friend ostream &operator << (ostream &os, GrafOr &grO);
-	///friend GrafOr &operator + (GrafOr &grO1, GrafOr &grO2);
+	bool Null() { rad == NULL ? true : false; }
 };
 
-GrafOr::GrafOr()
+
+istream &operator >> (istream &is, ABC &abc)
 {
-	this->n = 0;
-	this->m = 0;
+	int x;
+	char t;
+	Nod *n;
 
-	vector <int> v;
-	vfAdiac.push_back(v);
-}
+	if (abc.rad != NULL)
+		delete &abc;
 
-int **GrafOr::getMat()
-{
-	int **matAdiac;
-	matAdiac = new int*[this->n + 1];
+	cout << "Cititi pana la introducerea lui 0: ";
+	is >> x;
+	if (!x)
+		return is;
+	abc.rad = new Nod(x);
 
-	for (int i = 1; i <= this->n; i++)
+	is >> x;
+	while (x)
 	{
-		matAdiac[i] = new int[this->n + 1];
-
-		for (int j = 1; j <= this->n + 1; j++)
-			matAdiac[i][j] = 0;
-
-		for (int j = 0; j < vfAdiac[i].size(); j++)
-			matAdiac[i][vfAdiac[i][j]] = 1;
-	}
-	return matAdiac;
-}
-
-void GrafOr::addNod()
-{
-	this->n++;
-
-	vector <int> v;
-	vfAdiac.push_back(v);
-}
-
-void GrafOr::bfs(int x)
-{
-	cout << "BFS din nodul nr " << x << ": ";
-
-	bool *viz = new bool[this->n + 1];
-	for (int i = 1; i <= this->n; i++)
-		viz[i] = 0;
-
-	viz[x] = 1;
-	queue <int> qu;
-	qu.push(x);
-
-	while (!qu.empty())
-	{
-		x = qu.front();
-		cout << x << " ";
-		qu.pop();
-
-		for (int i = 0; i < vfAdiac[x].size(); i++)
-			if (!viz[vfAdiac[x][i]])
-			{
-				viz[vfAdiac[x][i]] = 1;
-				qu.push(vfAdiac[x][i]);
-			}
-	}
-	cout << endl;
-}
-
-void GrafOr::dfs(int x)
-{
-	bool *viz = new bool[this->n + 1];
-	for (int i = 1; i <= this->n; i++)
-		viz[i] = 0;
-
-	cout << "DFS din nodul nr " << x << ": ";
-	dfsUtil(x, viz);
-	cout << endl;
-}
-
-void GrafOr::reSetGrOr(int x)
-{
-	this->n = x;
-	this->m = 0;
-
-	for (int i = 0; i < vfAdiac.size(); i++)
-		vfAdiac[i].clear();
-	vfAdiac.clear();
-
-	vector <int> v;
-	for (int i = 0; i <= x; i++)
-		vfAdiac.push_back(v);
-};
-
-void GrafOr::addDrum(int x, int y)
-{
-	if (x > this->n || y > this->n || x == y)
-	{
-		cout << "\nEroare la adaugarea drumului\n";
-		return;
-	}
-	for (int i = 0; i < this->vfAdiac[x].size(); i++)
-		if (this->vfAdiac[x][i] == y)
-			return;
-
-	this->m++;
-	this->vfAdiac[x].push_back(y);
-}
-
-void GrafOr::dfsUtil(int x, bool viz[])
-{
-	viz[x] = 1;
-	cout << x << " ";
-
-	for (int i = 0; i < vfAdiac[x].size(); i++)
-		if (!viz[vfAdiac[x][i]])
-			dfsUtil(vfAdiac[x][i], viz);
-}
-
-bool GrafOr::isTareConex()
-{
-	if (compTareConexe().size() == 1)
-		return true;
-	else
-		return false;
-}
-
-bool GrafOr::drumFromTo(int x, int y)
-{
-	if (x == y)
-		return false;
-
-	bool *viz = new bool[this->n];
-	for (int i = 1; i <= this->n; i++)
-		viz[i] = 0;
-
-	viz[x] = 1;
-	queue <int> qu;
-	qu.push(x);
-
-	while (!qu.empty())
-	{
-		x = qu.front();
-		qu.pop();
-
-		for (int i = 0; i < vfAdiac[x].size(); i++)
-			if (!viz[vfAdiac[x][i]])
-			{
-				if (vfAdiac[x][i] == y)
-					return true;
-				viz[vfAdiac[x][i]] = 1;
-				qu.push(vfAdiac[x][i]);
-			}
-	}
-	return false;
-}
-
-vector <vector <int>> GrafOr::compTareConexe()
-{
-	bool *check = new bool[this->n + 1];
-
-	vector <vector <int>> comp;
-	vector <int> v;
-
-	for (int i = 1; i <= n; i++)
-		check[i] = 0;
-
-	int checking = 1;
-	int numC = 0;
-
-	while (checking)
-	{
-		comp.push_back(v);
-		comp[numC].push_back(checking);
-		check[checking] = 1;
-		for (int i = 1; i <= this->n; i++)
-		{
-			if (!check[i] && drumFromTo(checking, i) && drumFromTo(i, checking))
-			{
-				comp[numC].push_back(i);
-				check[i] = 1;
-			}
-		}
-		checking = 0;
-		for (int i = 1; i <= this->n; i++)
-		{
-			if (check[i] == 0)
-			{
-				checking = i;
-				break;
-			}
-		}
-		numC++;
+		abc.add(x, abc.rad);
+		cin >> x;
 	}
 
-	return comp;
-}
-
-istream &operator >> (istream &is, GrafOr &grO)
-{
-	int n, m, x, y;
-
-	is >> n >> m;
-
-	grO.reSetGrOr(n);
-
-	for (int i = 0; i < m; i++)
-	{
-		is >> x >> y;
-		grO.addDrum(x, y);
-	}
 	return is;
 }
 
-ostream &operator << (ostream &os, GrafOr &grO)
+ostream &operator << (ostream &os, ABC &abc)
 {
-	os << grO.getN() << " noduri si " << grO.getM() << " muchii\n";
-
-	for (int i = 1; i <= grO.getN(); i++)
+	if (abc.rad == NULL)
 	{
-		os << "nodul " << i << " are drum catre: ";
-
-		for (int j = 0; j < grO.getDrumuri(i).size(); j++)
-			os << grO.getDrumuri(i)[j] << " ";
-
-		os << "\n";
+		cout << "Arborele este nul.";
+		return os;
 	}
+
+	cout << "Arborele afisat in ordine: \n";
+	abc.srd(abc.rad);
+	cout << endl;
+
 	return os;
 }
 
-GrafOr operator + (GrafOr &grO1, GrafOr &grO2)
-{
-	GrafOr rez(max(grO1.getN(), grO2.getN()));
-
-	for (int i = 1; i <= grO1.getN(); i++)
-		for (int j = 0; j < grO1.getDrumuri(i).size(); j++)
-			rez.addDrum(i, grO1.getDrumuri(i)[j]);
-
-	for (int i = 1; i <= grO2.getN(); i++)
-		for (int j = 0; j < grO2.getDrumuri(i).size(); j++)
-			rez.addDrum(i, grO2.getDrumuri(i)[j]);
-
-	return rez;
-}
 
 int main()
 {
-	int t = 1, x;
-	ifstream f("date.txt");
-	GrafOr graf;
-	cout << "Cititi graful (nr noduri, nr muchii, legaturi intre noduri):\n";
-	cin >> graf;
-	//f >> graf;
-	cout << "Graf citit.\n\n";
-	while (t)
+	int t, nr, nr2;
+	ABC abc[10];
+
+	do
 	{
-		cout << "1. Afisare graf\n2. DFS\n3. BFS\n";
-		cout << "4. Matrice de adiacenta\n5. Componente tare conexe\n";
-		cout << "6. Graf tare conex?\n7. Adunare cu alt graf + afisare\n";
-		cout << "8. Adaugare nod\n9. Adaugare arc\n0. Exit\n";
+		cout << "1. Citire arbore binar de cautare\n";
+		cout << "2. Afisare arbore binar de cautare\n";
+		cout << "3. Copiere arbore binar de cautare\n";
+		cout << "0. Exit\n";
 		cin >> t;
 		switch (t)
 		{
 		case 1:
 		{
-			cout << graf;
+			cout << "Nr arborelui (0-9): ";
+			cin >> nr;
+			cin >> abc[nr];
 		}
 		break;
 		case 2:
 		{
-			cout << "Nodul din care sa inceapa: ";
-			cin >> x;
-			if (x > 0 && x <= graf.getN())
-				graf.dfs(x);
+			cout << "Nr arborelui (0-9): ";
+			cin >> nr;
+			if (!abc[nr].Null())
+				cout << abc[nr];
+			else
+				cout << "Nu a fost citit inca\n";
 		}
 		break;
 		case 3:
 		{
-			cout << "Nodul din care sa inceapa: ";
-			cin >> x;
-			if (x > 0 && x <= graf.getN())
-				graf.bfs(x);
-		}
-		break;
-		case 4:
-		{
-			int **mat = graf.getMat();
-			for (int i = 1; i <= graf.getN(); i++)
+			cout << "Nr arborelui destinatie (0-9): ";
+			cin >> nr;
+			cout << "Nr arborelui copiat (0-9): ";
+			cin >> nr2;
+			if (!abc[nr2].Null())
 			{
-				for (int j = 1; j <= graf.getN(); j++)
-					cout << mat[i][j] << " ";
-				cout << endl;
+				abc[nr] = abc[nr2];
+				cout << "Arborele a fost copiat\n";
 			}
-		}
-		break;
-		case 5:
-		{
-			vector <vector <int>> ctc = graf.compTareConexe();
-			for (int i = 0; i < ctc.size(); i++)
-			{
-				cout << "Componenta nr " << i+1 << ": ";
-				for (int j = 0; j < ctc[i].size(); j++)
-					cout << ctc[i][j] << " ";
-				cout << endl;
-			}
-		}
-		break;
-		case 6:
-		{
-			if (graf.isTareConex())
-				cout << "Graful este tare conex.\n";
 			else
-				cout << "Graful nu este tare conex\n";
-		}
-		break;
-		case 7:
-		{
-			GrafOr graf2;
-			cout << "Cititi graful 2:\n";
-			cin >> graf2;
-			//f >> graf2;
-			GrafOr graf3 = graf + graf2;
-			cout << "Graful 1 + Graful 2 = \n";
-			cout << graf3;
-		}
-		break;
-		case 8:
-		{
-			graf.addNod();
-			x = graf.getN();
-			cout << "Nodul nr " << x << " a fost adaugat.\n";
-		}
-		break;
-		case 9:
-		{
-			int y;
-			cout << "Nodul sursa si nodul destinatie: ";
-			cin >> x >> y;
-			graf.addDrum(x, y);
-			cout << "Arcul a fost adaugat.\n";
+				cout << "Nu a fost citit inca\n";
 		}
 		break;
 		case 0:
 			break;
-		default: cout << "Optiune invalida.\n";
+		default: cout << "Optiune invalida\n";
 			break;
 		}
-		cout << endl;
-	}
-	f.close();
+		cout << "\n";
+	} while (t);
 	return 0;
 }
